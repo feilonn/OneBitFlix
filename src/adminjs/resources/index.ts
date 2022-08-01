4b2d50d8df7b81b21e3e6ca2e52b77babcf7eba4
Raul Correia <raulvinicius245@gmail.com>
2022-08-01 17:30:26 -0300

    Criando migration e model para Course. Adicinado resource ao AdminJS

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,11 +1,20 @@
-import { categoryResourceOptions } from './category';
-import { ResourceWithOptions } from 'adminjs';
-import { Category } from '../../models';
+import { ResourceWithOptions } from "adminjs";
+
+//Configurações Resource AdminJS
+import { categoryResourceOptions } from "./category";
+import { courseResourceOptions } from "./course";
+
+//Models e Relacionamentos
+import { Category, Course } from "../../models";
 
 //ARQUIVO PARA REUNIR TODOS OS RESOURCES PARA O ADMINJS GERENCIAR
 export const adminJsResources: ResourceWithOptions[] = [
-    {
-        resource: Category,
-        options: categoryResourceOptions
-    }
-]
+  {
+    resource: Category,
+    options: categoryResourceOptions,
+  },
+  {
+    resource: Course,
+    options: courseResourceOptions
+  },
+];
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,14 @@
+import { Course } from './Course';
 import { Category } from './Category'
 
 //ARQUIVO QUE REUNE TODOS OS MODELS SEQUELIZE DA APLICAÇÃO
+//E SEUS RELACIONAMENTOS
+
+//Relacionamento Many to One: Category -> Course
+Category.hasMany(Course);
+Course.belongsTo(Category);
+
 export {
-  Category
+  Category,
+  Course
 }
