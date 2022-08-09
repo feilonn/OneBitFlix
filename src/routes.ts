88c4b4624d00afb319b931ffe54fed684ca0d2bf
Raul Correia <raulvinicius245@gmail.com>
2022-08-08 22:55:52 -0300

    Video Streaming

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 //Controllers
 import { coursesController } from './controllers/coursesController';
 import { categoriesController } from './controllers/categoriesController';
+import { episodesController } from './controllers/episodesController';
 
 export const router = express.Router();
 
@@ -14,3 +15,6 @@ router.get('/courses/featured', coursesController.featuredCourses);
 router.get('/courses/newest', coursesController.newestCourses);
 router.get('/courses/search', coursesController.searchCourses);
 router.get('/courses/:id', coursesController.coursesWithEpisodes);
+
+//Courses Videos
+router.get('/episodes/stream', episodesController.stream);
