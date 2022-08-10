3a294ad437554c2f787d08f40987d6c40bf52da4
Raul Correia <raulvinicius245@gmail.com>
2022-08-09 22:24:23 -0300

    Protegendo rotas

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { coursesController } from './controllers/coursesController';
 import { categoriesController } from './controllers/categoriesController';
 import { episodesController } from './controllers/episodesController';
 import { authController } from './controllers/authController';
+import { ensureAuth } from './middlewares/auth';
 
 export const router = express.Router();
 
@@ -12,14 +13,14 @@ router.post('/auth/register', authController.register);
 router.post('/auth/login', authController.login)
 
 //Categories Routes
-router.get('/categories', categoriesController.index);
-router.get('/categories/:id', categoriesController.categoryWithCourses);
+router.get('/categories', ensureAuth, categoriesController.index);
+router.get('/categories/:id', ensureAuth, categoriesController.categoryWithCourses);
 
 //Courses Routes
-router.get('/courses/featured', coursesController.featuredCourses);
+router.get('/courses/featured', ensureAuth, coursesController.featuredCourses);
 router.get('/courses/newest', coursesController.newestCourses);
-router.get('/courses/search', coursesController.searchCourses);
-router.get('/courses/:id', coursesController.coursesWithEpisodes);
+router.get('/courses/search', ensureAuth, coursesController.searchCourses);
+router.get('/courses/:id', ensureAuth, coursesController.coursesWithEpisodes);
 
 //Courses Videos
 router.get('/episodes/stream', episodesController.stream);
