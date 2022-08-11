2cea8fa4a8b3ea56648fd8a080236e718ef7eb66
Raul Correia <raulvinicius245@gmail.com>
2022-08-10 23:38:17 -0300

    CORS

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import { adminJs, adminJsRouter } from './adminjs/index';
+import cors from "cors";
 import express from "express";
 import { sequelize } from "./database";
 import { router } from './routes';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
